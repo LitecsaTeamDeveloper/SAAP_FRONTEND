aff180f922f146f026235bbf27e380cd53cd8390
Chaper <ijchaper@hotmail.com>
2024-06-04 19:27:01 -0600

    version 1 de inventario finalizado

--- src/app/shared/constant.ts
+++ src/app/shared/constant.ts
@@ -1,5 +1,7 @@
-// export const API_URL = 'http://localhost:83/api';  //api en IIS local mi compu puej
+ // export const API_URL = 'http://localhost:83/api';  //api en IIS local mi compu puej
   
-export const API_URL = 'http://localhost:5057/api';      //api en mi local de desarrollo
+  export const API_URL = 'http://localhost:5057/api';      //api en mi local de desarrollo
 
 //export const API_URL = 'http://www.pipetubo.somee.com/api';  // api en website externo de prueba
+
+//export const API_URL = 'http://192.168.10.250:8888/api';      //api en servidor litecsa
